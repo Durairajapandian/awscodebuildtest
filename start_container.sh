e5a8798a192e19bfe4f4fd1a29571b778ef67154
Durairajapandian <95368872+Durairajapandian@users.noreply.github.com>
2024-03-26 16:18:14 +0530

    start changes

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull 9677778749/python-application:latest2
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 9677778749/python-application:latest2
