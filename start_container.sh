95c9c258de02f1317cfb7a1505f688ea184cf97e
Durairajapandian <95368872+Durairajapandian@users.noreply.github.com>
2024-05-27 17:31:49 +0530

    test

--- start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull 9677778749/python-application:latest2
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 9677778749/python-application:latest2
-
