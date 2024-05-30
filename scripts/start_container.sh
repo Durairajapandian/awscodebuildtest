b3c95b637445ff7eac573d524341fd15691bcef3
Durairajapandian <95368872+Durairajapandian@users.noreply.github.com>
2024-05-30 15:31:15 +0530

    stop

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,20 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull 9677778749/9677778749
 
+# Check if any containers are running
+if [ $(docker ps -q | wc -l) -gt 0 ]; then
+    # Stop all running containers
+    docker stop $(docker ps -q)
+    
+    # Move containers to another folder
+    for container_id in $(docker ps -a -q); do
+        docker inspect --format='{{.Name}}' $container_id | cut -c2- | xargs -I {} docker commit $container_id moved_{}
+        docker rm $container_id
+    done
+
+    echo "Containers moved successfully."
+else
+    echo "No containers running."
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 9677778749/9677778749
