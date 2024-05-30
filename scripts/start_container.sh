ed9d26e7d51cc54ded6101b8497f80d81d60554c
Durairajapandian <95368872+Durairajapandian@users.noreply.github.com>
2024-05-30 11:51:52 +0530

    docker name

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull 9677778749/9677778749:latest
+docker pull 9677778749/9677778749
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 9677778749/9677778749:latest
+docker run -d -p 5000:5000 9677778749/9677778749
