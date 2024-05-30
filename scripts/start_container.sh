3041f560841a4368fea9eed0166e8de1ac3a33cf
Durairajapandian <95368872+Durairajapandian@users.noreply.github.com>
2024-05-30 15:45:55 +0530

    docker

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,19 +6,10 @@ docker pull 9677778749/9677778749
 
 # Check if any containers are running
 if [ $(docker ps -q | wc -l) -gt 0 ]; then
-    # Stop all running containers
-    docker stop $(docker ps -q)
-    
-    # Move containers to another folder
-    for container_id in $(docker ps -a -q); do
-        docker inspect --format='{{.Name}}' $container_id | cut -c2- | xargs -I {} docker commit $container_id moved_{}
-        docker rm $container_id
-    done
-
-    echo "Containers moved successfully."
+    # Force delete all running containers
+    docker rm -f $(docker ps -aq)
 else
-    echo "No containers running."
-
+    echo "No containers running"
 fi
 
 # Run the Docker image as a container
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
+set -e
 
-# Check if any containers are running
-if [ $(docker ps -q | wc -l) -gt 0 ]; then
-    # Stop all running containers
-    docker stop $(docker ps -q)
-    
-    # Move containers to another folder
-    for container_id in $(docker ps -a -q); do
-        docker inspect --format='{{.Name}}' $container_id | cut -c2- | xargs -I {} docker commit $container_id moved_{}
-        docker rm $container_id
-    done
-
-    echo "Containers moved successfully."
-else
-    echo "No containers running."
-fi
+# Stop the running container (if any)
+echo "Hi"
