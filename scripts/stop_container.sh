68e4f839225f3d8ed2b80e8a2d1d4d2ba6a44af9
Durairajapandian <95368872+Durairajapandian@users.noreply.github.com>
2024-05-30 15:22:31 +0530

    docker change

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Check if any containers are running
+if [ $(docker ps -q | wc -l) -gt 0 ]; then
+    # Stop all running containers
+    docker stop $(docker ps -q)
+    
+    # Move containers to another folder
+    for container_id in $(docker ps -a -q); do
+        docker inspect --format='{{.Name}}' $container_id | cut -c2- | xargs -I {} docker commit $container_id moved_{}
+        docker rm $container_id
+    done
 
+    echo "Containers moved successfully."
+else
+    echo "No containers running."
+fi
