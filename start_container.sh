48abf333c62361c15e9457c13f10258ba208b197
Durairajapandian <95368872+Durairajapandian@users.noreply.github.com>
2024-06-07 00:19:04 +0530

    try

---
+++ start_container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Check if any containers are running
+if [ $(docker ps -q | wc -l) -gt 0 ]; then
+    # Force delete all running containers
+    docker rm -f $(docker ps -aq)
+else
+    echo "No containers running"
+fi
+
+# Pull the Docker image from Docker Hub
+docker pull 9677778749/9677778749
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 9677778749/9677778749
